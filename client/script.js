f1d55a5c34cc9d43cb000291ede823af6f141c7e
George Park <gminator@gmail.com>
2022-12-30 11:19:44 -0800

    updates

--- client/script.js
+++ client/script.js
@@ -84,7 +84,7 @@ const handleSubmit = async (e) => {
 
   // fetch data from server
 
-  const response = await fetch('http://localhost:3000', {
+  const response = await fetch('https://chatbot-zx0z.onrender.com', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json'
